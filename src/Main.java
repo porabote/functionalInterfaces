aa51d88b904cbd480f92fc07375e13149ca78c81
maksimov <dmaksimov@vtb.ru>
2023-03-13 20:24:45 +0300

    calc

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(0,1);
+
+        // Нельзя делить на ноль
+        if (b > 0) {
+            int c = calc.devide.apply(a, b);
+            calc.println.accept(c);
+        }
+    }
+}
