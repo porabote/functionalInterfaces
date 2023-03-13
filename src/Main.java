ca759df540ca3482c6537c1e8403eab43f693f6b
maksimov <dmaksimov@vtb.ru>
2023-03-13 21:24:22 +0300

    worker

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,22 @@ public class Main {
     public static void main(String[] args) {
 
         // Calculator
-//        Calculator calc = Calculator.instance.get();
-//
-//        int a = calc.plus.apply(1, 2);
-//        int b = calc.minus.apply(0,1);
-//
-//        // Нельзя делить на ноль
-//        if (b > 0) {
-//            int c = calc.devide.apply(a, b);
-//            calc.println.accept(c);
-//        }
+        Calculator calc = Calculator.instance.get();
 
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(0,1);
+
+        // Нельзя делить на ноль
+        if (b > 0) {
+            int c = calc.devide.apply(a, b);
+            calc.println.accept(c);
+        }
 
         // Worker
         Worker.OnTaskDoneListener listener = System.out::println;
+        Worker.OnTaskErrorListener errorCallback = System.out::println;
 
-        Worker worker = new Worker(listener);
+        Worker worker = new Worker(listener, errorCallback);
         worker.start();
 
     }
--- src/Worker.java
+++ src/Worker.java
@@ -1,9 +1,11 @@
 public class Worker {
 
     private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
 
-    public Worker(OnTaskDoneListener callback) {
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
         this.callback = callback;
+        this.errorCallback = errorCallback;
     }
 
     @FunctionalInterface
@@ -11,9 +13,19 @@ public class Worker {
         void onDone(String result);
     }
 
+    @FunctionalInterface
+    public interface OnTaskErrorListener {
+        void onError(String result);
+    }
+
     public void start() {
         for (int i = 0; i < 100; i++) {
-            callback.onDone("Task " + i + " is done");
+
+            if (i == 33) {
+                errorCallback.onError("Task " + i + " has error");
+            } else {
+                callback.onDone("Task " + i + " is done");
+            }
         }
     }
 
