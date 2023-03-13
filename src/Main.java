3321961a2ca6b1390857b7ac0a2330ef01d1b1e4
maksimov <dmaksimov@vtb.ru>
2023-03-13 21:06:48 +0300

    worker

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
 public class Main {
     public static void main(String[] args) {
 
-        Calculator calc = Calculator.instance.get();
+        // Calculator
+//        Calculator calc = Calculator.instance.get();
+//
+//        int a = calc.plus.apply(1, 2);
+//        int b = calc.minus.apply(0,1);
+//
+//        // Нельзя делить на ноль
+//        if (b > 0) {
+//            int c = calc.devide.apply(a, b);
+//            calc.println.accept(c);
+//        }
 
-        int a = calc.plus.apply(1, 2);
-        int b = calc.minus.apply(0,1);
 
-        // Нельзя делить на ноль
-        if (b > 0) {
-            int c = calc.devide.apply(a, b);
-            calc.println.accept(c);
-        }
+        // Worker
+        Worker.OnTaskDoneListener listener = System.out::println;
+
+        Worker worker = new Worker(listener);
+        worker.start();
+
     }
 }
--- src/Worker.java
+++ src/Worker.java
@@ -1,2 +1,20 @@
 public class Worker {
+
+    private OnTaskDoneListener callback;
+
+    public Worker(OnTaskDoneListener callback) {
+        this.callback = callback;
+    }
+
+    @FunctionalInterface
+    public interface OnTaskDoneListener {
+        void onDone(String result);
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            callback.onDone("Task " + i + " is done");
+        }
+    }
+
 }
